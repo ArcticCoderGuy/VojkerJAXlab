2f3e9d512e3c01021fdb8fa982343eda55ea0070
Markus Kaprio <markus.kaprio@edunord.se>
2026-02-24 15:07:34 +0200

    Defined the shapes/axis 1 & 2  on JAX

--- DiMarkus_Pizza_Tensor_example.py
+++ DiMarkus_Pizza_Tensor_example.py
@@ -1,11 +1,15 @@
 Python jax.numpy as jnp
 
+# 1. Define the dimensions of our tensor (Pizza example) and with my "Lean JAX Jedi mindset" which is - perfect parts in, perfect parts out, no more, no less. 
+
+
+
   # Always mark-up: batch/row-size (n),  feature dimension (d), FYI -> number of batches (b), output dimension (k) 
   
-  # Tensor, the Pizza example: n = slices, d = toppings, b = pizzas, k =    
+  # Tensor-Pizza example: n = slices, d = toppings, b = pizzas, k =    
 
-n = 20
-d = 3
+n = 8 # every pizza has 8 slices
+d = 5 # every slice has 5 toppings (pepperoni, mushroom, onion, olive, pineapple)
 
 
 
